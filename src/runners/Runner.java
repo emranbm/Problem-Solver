02a5025acd3fb96ccfc86380f1e125e85ea13da8
Emran <emran.bm@gmail.com>
2016-10-31 14:11:34 +0330

    Prints path.

--- src/runners/Runner.java
+++ src/runners/Runner.java
@@ -3,6 +3,8 @@ package runners;
 import models.State;
 import solvers.Solver;
 
+import java.util.ArrayList;
+
 /**
  * Created by emran on 10/29/16.
  */
@@ -21,7 +23,30 @@ public class Runner extends Thread {
             goal = solver.tick();
         }
 
-        //TODO print nice result :)
-        System.out.println(goal.getId());
+        models.State state = goal;
+
+        ArrayList<models.State> path = new ArrayList<>();
+
+        while (state != null) {
+            path.add(state);
+            state = state.getParent();
+        }
+
+        path = reversePath(path);
+
+        printPath(path);
+    }
+
+    private static ArrayList<models.State> reversePath(ArrayList<models.State> path) {
+        ArrayList<models.State> reverse = new ArrayList<>();
+        for (int i = path.size() - 1; i >= 0; i--)
+            reverse.add(path.get(i));
+
+        return reverse;
+    }
+
+    private static void printPath(ArrayList<models.State> path) {
+        for (models.State state : path)
+            System.out.print(state.getId() + " ");
     }
 }
